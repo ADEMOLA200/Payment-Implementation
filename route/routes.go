8b73005a16676a7fc7e79e8663d3866a2b037f7d
Odukota Abdullahi Ademola <odukoyaabdullahi01@gmail.com>
2024-03-26 12:55:58 +0100

    Added new feature to the payment controller to handle additional payment methods.
    Implemented error handling in the paystack service to handle failed API requests more gracefully.
    
    Completed integration of frontend changes with the backend API.
    
    go mod
    
    go sum
    
    main
    
    models
    
    services
    
    log#

--- route/routes.go
+++ route/routes.go
@@ -3,12 +3,18 @@ package routes
 import (
 	"github.com/ADEMOLA200/Payment-Implementation/controller"
 	"github.com/ADEMOLA200/Payment-Implementation/middlewares"
+	"github.com/ADEMOLA200/Payment-Implementation/webhook"
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetupRoutes(app *fiber.App) {
-	// Setup routes
+    // Setup API routes
     api := app.Group("/api", middlewares.CorsMiddleware(), middlewares.LoggingMiddleware)
-
     api.Post("/makePayment", controller.MakePayment)
+	app.Post("/webhook/paystack", webhook.HandlePaystackWebhook) // Connect the webhook handler function to a route
+
+
+    // Setup webhook endpoint
+    // webhookGroup := app.Group("/webhook")
+    // webhookGroup.Post("/paystack", webhook.HandlePaystackWebhook)
 }
